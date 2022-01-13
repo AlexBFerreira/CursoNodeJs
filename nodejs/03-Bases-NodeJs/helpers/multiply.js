05380bcd441dd6cdbc468d81e51f5e79c857efe2
ct24637 <junioralexferreiraroa@gmail.com>
2022-01-13 15:08:04 -0400

    configuracion de git ignore

--- nodejs/03-Bases-NodeJs/helpers/multiply.js
+++ nodejs/03-Bases-NodeJs/helpers/multiply.js
@@ -1,24 +1,16 @@
 const fs = require('fs');
 
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let salida = "";
 
-const crearArchivo = (base, listar = false, limite = numeros) => {
-
-
+const crearArchivo = (base, listar = false, limite = 10) => {
     return new Promise((resolve, reject) => {
         for (let element = 1; element <= limite; element++) {
             salida += `${element} * ${base} = ${element * base}\n`;
         }
-
         listar ? console.log(salida) : null;
-
-        fs.writeFileSync(`Tabla-${base}.txt`, salida)
-
+        fs.writeFileSync(`./salida/Tabla-${base}.txt`, salida)
         salida != "" ? resolve(`Tabla-${base}.txt`) : reject("No se pudo crear el archivo");
     })
-
-
 }
 
 module.exports = {
