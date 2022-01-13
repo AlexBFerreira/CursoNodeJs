7dcc10a40c774f67738bd5f3ab037676db80309d
ct24637 <junioralexferreiraroa@gmail.com>
2022-01-13 14:16:52 -0400

    adicion del modulo 3 con algunos ejercicios

---
+++ nodejs/03-Bases-NodeJs/config/yargs.js
@@ -0,0 +1,26 @@
+const argv = require('yargs')
+    .options({
+        'b': {
+            alias: 'base',
+            type: 'number',
+            describe: 'base is the number for the multiplication table ',
+        }, 'l': {
+            alias: 'listar',
+            type: 'boolean',
+            describe: 'Show the result of the comand line',
+            default: false,
+        }, 'h': {
+            alias: 'hasta',
+            type:'number',
+            describe: 'Límite de veces a multiplicar por la base',
+            default: 10,
+        }
+    })
+    .check((argv, options) => {
+        if (isNaN(argv.b)) {
+            throw "The base must be a number";
+        }
+        return true;
+    }).argv;
+
+module.exports = argv;
